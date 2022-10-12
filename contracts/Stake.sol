85567fd662c63a3186e4aa5552c6fac03c59193d
elcomendante <karol@finsane.com>
2022-10-12 11:13:32 +0100

    stake starts

---
+++ contracts/Stake.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Stake is AccessControl {}
