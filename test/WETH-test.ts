5e76253e8bfd03e6f7d5c0c183443eaf651f91cb
elcomendante <karol@finsane.com>
2022-10-14 19:48:44 +0100

    tested but hh is fucked tests

--- test/WETH-test.ts
+++ test/WETH-test.ts
@@ -15,27 +15,38 @@ describe("CONTRACT:WETH", function () {
   }
 
   describe("DEPOSIT-WITHDRAW", function () {
-    it("Should deposit correctly", async function () {
-      const { weth, owner, acc1 } = await loadFixture(deployWETH);
+    it("Should deposit and withdraw correctly", async function () {
+      const { weth, acc1, acc2 } = await loadFixture(deployWETH);
 
       // 1. contract has zero ethers
       expect(
         formatEther(await ethers.provider.getBalance(weth.address))
       ).to.equal(formatEther("0"));
 
+      expect(await weth.totalSupply()).to.equal(0);
+
       // 2.acc1 deposits 10 ETH
-      // weth.connect(acc1).deposit({ value: parseEther("1")
-    });
 
-    it("Should withdraw correctly", async function () {
-      const { weth, owner, acc1 } = await loadFixture(deployWETH);
+      expect(await weth.connect(acc1).deposit({ value: parseEther("10") }))
+        .emit(weth, "Transfer")
+        .withArgs(acc1.address, "0x", 10);
 
-      const acc1Balance = await weth.balanceOf(acc1.address);
-      const totalSupply = await weth.totalSupply();
+      // Below failes ?????
+      //   expect(
+      //     formatEther(await ethers.provider.getBalance(weth.address))
+      //   ).to.be.equal(10);
+      // expect(await weth.balanceOf(acc1.address)).to.equal(10);
 
-      // revert
-      await expect(weth.connect(acc1).withdraw(1)).to.be.revertedWith(
-        "only owner"
+      expect(await weth.totalSupply()).to.equal(
+        await weth.balanceOf(acc1.address)
+      );
+
+      expect(await weth.connect(acc1).withdraw(10))
+        .emit(weth, "Transfer")
+        .withArgs("0x", acc1.address, 10);
+
+      expect(await weth.totalSupply()).to.equal(
+        await weth.balanceOf(acc1.address)
       );
     });
   });
